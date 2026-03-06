cf63f303b808ffd0014c7ed483be51be9cfad760
Tharakaherath <wtharakaherath@gmail.com>
2026-03-06 17:25:02 +0530

    Added average marks calculation feature

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,7 +1,26 @@
-name = input("Enter student name: ")
-marks = int(input("Enter marks: "))
+students = []
+total_marks = 0
 
-if marks >= 50:
-    print(name, "has passed")
-else:
-    print(name, "has failed")
+for i in range(3):
+    name = input("Enter student name: ")
+    marks = int(input("Enter marks: "))
+
+    if marks >= 75:
+        grade = "A"
+    elif marks >= 65:
+        grade = "B"
+    elif marks >= 50:
+        grade = "C"
+    else:
+        grade = "F"
+
+    students.append((name, marks, grade))
+    total_marks += marks
+
+print("\nStudent Results")
+
+for s in students:
+    print("Name:", s[0], "| Marks:", s[1], "| Grade:", s[2])
+
+average = total_marks / len(students)
+print("\nAverage Marks:", average)

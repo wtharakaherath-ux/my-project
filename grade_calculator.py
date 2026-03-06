0070543cc3911a0102795db2c35461994c6478ab
Tharakaherath <wtharakaherath@gmail.com>
2026-03-06 17:06:50 +0530

    Added grade classification system

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,7 +1,15 @@
 name = input("Enter student name: ")
 marks = int(input("Enter marks: "))
 
-if marks >= 50:
-    print(name, "has passed")
+if marks >= 75:
+    grade = "A"
+elif marks >= 65:
+    grade = "B"
+elif marks >= 50:
+    grade = "C"
 else:
-    print(name, "has failed")
+    grade = "F"
+
+print("Student:", name)
+print("Marks:", marks)
+print("Grade:", grade)
